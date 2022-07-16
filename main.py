193c953ef5bce3e8a10d19d9700e2854acb4662c
Nazif <nzfishrak60@gmail.com>
2022-07-16 15:30:19 +0600

    Added difficulty level and play again feature

--- main.py
+++ main.py
@@ -4,44 +4,72 @@ import time
 from food import Food
 from scoreboard import ScoreBoard
 from snake_body import Snake
-screen = turtle.Screen()
-screen.setup(600,600)
-screen.bgcolor('black')
-screen.title("Snake Game")
-screen.tracer(0)
-snake = Snake()
-food = Food()
-score_board = ScoreBoard()
 
-game_is_on = True
-screen.listen()
-screen.onkey(snake.right, "Right")
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.down, "Down")
-while game_is_on:
-    screen.update()
-    snake.move()
-    time.sleep(0.05)
 
-    # Detect collision with food
 
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.grow()
-        score_board.increase_score()
+
+
+
+
+def main():
+    # *************************************** SCREEN SETUP*************************
+    screen = turtle.Screen()
+    screen.clear()
+    screen.setup(600,600)
+    screen.bgcolor('black')
+    screen.title("Snake Game")
+    screen.tracer(0)
     
-    x_cor = snake.head.xcor()
-    y_cor = snake.head.ycor()
-    if x_cor>=290 or x_cor <=-290 or y_cor>=290 or y_cor<=-290:
-        game_is_on = False
-        score_board.game_over()
+    # **********************Setting time delay of graphics based on easy/hard game level**************
+    user_difficulty = screen.textinput("Choose Difficulty Level", "easy/hard")
+    if user_difficulty.lower() == 'easy':
+        user_difficulty = 0.1
+    else:   
+        user_difficulty = 0.05
+    
+    # ***************************CREATING OBJECTS***********************************
+    snake = Snake()
+    food = Food()
+    score_board = ScoreBoard()
 
-    for box in snake.boxes[1::]:
-        if snake.head.distance(box) <= 15:
+
+    # **********************GAME FLOW LOGIC*************************************
+    game_is_on = True
+    screen.listen()
+    screen.onkey(snake.right, "Right")
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.down, "Down")
+    while game_is_on:
+        screen.update()
+        snake.move()
+        time.sleep(user_difficulty)
+
+        # Detect collision with food
+
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.grow()
+            score_board.increase_score()
+        
+        # Detect collison with wall
+        x_cor = snake.head.xcor()
+        y_cor = snake.head.ycor()
+        if x_cor>=290 or x_cor <=-290 or y_cor>=290 or y_cor<=-290:
             game_is_on = False
             score_board.game_over()
-            
+        # Detect collision with own body
+        for box in snake.boxes[1::]: # Ignores the first posistion of the snake head
+            if snake.head.distance(box) <= 15:
+                game_is_on = False
+                score_board.game_over()
+    play_again = screen.textinput("Do you want to play again?", "yes/no")
+    if play_again.lower() == 'yes':
+        main()
+    else:   
+        pass
+
+main()
 
 
 
@@ -49,6 +77,6 @@ while game_is_on:
 
 
 
-screen.exitonclick()
+
 
 
